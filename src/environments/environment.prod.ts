672ec1b6febe62d43403b4fff7c1ea2cd5caeb03
kidagine <36992613+kidagine@users.noreply.github.com>
2020-03-22 22:11:43 +0100

    Added firebase and firebase functions.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA-3q6eG2mTZs-UJ3XSA3vW60fowy4clEk",
+    authDomain: "productsite-911f7.firebaseapp.com",
+    databaseURL: "https://productsite-911f7.firebaseio.com",
+    projectId: "productsite-911f7",
+    storageBucket: "productsite-911f7.appspot.com",
+    messagingSenderId: "1006163269731",
+    appId: "1:1006163269731:web:be7060836dbe42f53d8056",
+    measurementId: "G-HQQ81ZTRG5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA-3q6eG2mTZs-UJ3XSA3vW60fowy4clEk",
+    authDomain: "productsite-911f7.firebaseapp.com",
+    databaseURL: "https://productsite-911f7.firebaseio.com",
+    projectId: "productsite-911f7",
+    storageBucket: "productsite-911f7.appspot.com",
+    messagingSenderId: "1006163269731",
+    appId: "1:1006163269731:web:be7060836dbe42f53d8056",
+    measurementId: "G-HQQ81ZTRG5"
+  }
 };
 
 /*
