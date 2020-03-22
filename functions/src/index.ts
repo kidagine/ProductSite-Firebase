ff1ab328b3c11dd4134bf9dc96c8310a83f7f045
kidagine <36992613+kidagine@users.noreply.github.com>
2020-03-22 23:00:20 +0100

    Setup functions index.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { DependencyFactory } from './dependency-factory';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require("../secret.json")
+const dependencyFactory = new DependencyFactory
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://productsite-911f7.firebaseio.com"
+});
